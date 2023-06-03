b0992d31a886e3401a7861d98ce326f5cdd77a9a
Nick Sacco <nicksacco10@gmail.com>
2023-06-02 19:11:47 -0700

    feat: created landing page for app with links

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/", to: "welcome#index"
+  
   get '/merchants/:id/dashboard', to: 'merchants#show', as: 'merchant_dashboard'
 
   get "/admin", to: "admin#index"
