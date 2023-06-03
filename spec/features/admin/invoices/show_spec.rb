41f0e372a314bc8d33228b9ee2e37074cb3b9778
Steven Stackhouse <stackmm@gmail.com>
2023-06-03 15:45:00 -0600

    refactored tests

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -10,32 +10,20 @@ RSpec.describe "Admin Invoice Show Page" do
 
   describe "Admin Invoice Details" do
     # User Story 33
-    it "displays the invoice ID number at the top" do
+    it "displays the invoice details" do
       visit admin_invoice_path(@invoice_1)
       save_and_open_page
-      
-      expect(page).to have_content("Invoice ##{@invoice_1.id}")
-      expect(page).to_not have_content("Invoice ##{@invoice_2.id}")
+      within("#invoice-details") do
+        expect(page).to have_content("Invoice ##{@invoice_1.id}")
+        expect(page).to have_content("Status: #{@invoice_1.status}")
+        expect(page).to have_content("Created on: #{@invoice_1.created_at.strftime("%A, %B %d, %Y")}")
+
+        expect(page).to have_content("Customer:")
+        expect(page).to have_content("#{@customer_1.first_name} #{@customer_1.last_name}")
+        expect(page).to_not have_content("#{@customer_2.first_name} #{@customer_2.last_name}")
+
+        expect(page).to_not have_content("Invoice ##{@invoice_2.id}")
+      end
     end
-
-    it "displays the invoice status" do
-      visit admin_invoice_path(@invoice_1)
-
-      expect(page).to have_content("Status: #{@invoice_1.status}")
-    end
-
-    it "displays the created_at date in the format 'Weekday, Month Day, Year' " do
-      visit admin_invoice_path(@invoice_1.created_at)
-
-      expect(page).to have_content("Created on: #{@invoice_1.created_at.strftime("%A, %B %-d, %Y")}")
-    end
-
-    it "displays the Invoice's customer first and last name" do
-      visit admin_invoice_path(@invoice_1)
-
-      expect(page).to have_content("Customer:")
-      expect(page).to have_content("#{@customer_1.first_name} #{@customer_1.last_name}")
-      expect(page).to_not have_content("#{@customer_2.first_name} #{@customer_2.last_name}")
-    end 
   end
 end
