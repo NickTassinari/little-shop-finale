3e03d69b08519803c8da1522524904c8e24dc0ce
Nick Sacco <nicksacco10@gmail.com>
2023-06-03 10:00:44 -0700

    feat: added new action to merch controller and created view file

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -9,6 +9,10 @@ class Admin::MerchantsController < ApplicationController
     @merchant = Merchant.find(params[:id])
   end
 
+  def new
+    
+  end
+
   def edit
     @merchant = Merchant.find(params[:id])
   end
