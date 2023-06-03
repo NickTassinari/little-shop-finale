41c5fd21ef8520baf36b21b6794afc6f9d6be76e
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-06-02 20:35:40 -0600

    comment out

--- spec/features/merchants/items/show_spec.rb
+++ spec/features/merchants/items/show_spec.rb
@@ -28,12 +28,12 @@ RSpec.describe "Merchant Items Show Page" do
     #User Story 8
     it "displays link to update item" do
       visit merchant_item_path(@merchant_1, @item_1)
-      save_and_open_page
+      # save_and_open_page
 
       expect(page).to have_content(@item_1.description)
       expect(page).to have_content(@item_1.unit_price)
       expect(page).to have_link("Update #{@item_1.name}")
-      expect(page).to_not have_content(@item_2.name)
+      expect(page).to_not have_content(@item_3.name)
     end
 
     it "when visitor clicks link they're taken to the edit page, it displays a form filled in with existing item info" do
