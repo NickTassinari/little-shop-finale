a163b0a645e5ce4897b713c0fa900aef33da2fea
Nick Sacco <nicksacco10@gmail.com>
2023-06-03 09:58:20 -0700

    feat: new merchant link added to view and updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get "/admin", to: "admin#index"
 
   namespace :admin do
-    resources :merchants, only: [:index, :show, :edit, :update], controller: "merchants"
+    resources :merchants, except: [:destroy], controller: "merchants"
     resources :invoices, only: [:index, :show]
   end
 
