c48046acb3e0a70c4a45934e3d6a2d7268f4ad9b
Nick Tassinari <ntass7800@gmail.com>
2023-06-03 15:56:08 -0600

    tidy up a bit in here

--- spec/features/merchants/invoices/index_spec.rb
+++ spec/features/merchants/invoices/index_spec.rb
@@ -1,12 +1,7 @@
 require "rails_helper"
 
 RSpec.describe "Merchants Invoices Index" do 
-  # Merchant Invoices Index
-  # As a merchant,
-  # When I visit my merchant's invoices index (/merchants/:merchant_id/invoices)
-  # Then I see all of the invoices that include at least one of my merchant's items
-  # And for each invoice I see its id
-  # And each id links to the merchant invoice show page
+  # User Story 14 Merchant Invoices Index
   before(:each) do 
     @merchant = create(:merchant)
     
