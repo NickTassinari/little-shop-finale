776983822b17fa069159a98e1550d73751fb34ae
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-06-03 12:32:42 -0600

    test: Add tests for incomplete form redirect and error message

--- spec/features/merchants/items/edit_spec.rb
+++ spec/features/merchants/items/edit_spec.rb
@@ -12,17 +12,6 @@ RSpec.describe "Merchant Items Edit Page" do
     end
 
 # 8. Merchant Item Update
-
-# As a merchant,
-# When I visit the merchant show page of an item (/merchants/:merchant_id/items/:item_id)
-# I see a link to update the item information.
-# When I click the link
-# Then I am taken to a page to edit this item
-# And I see a form filled in with the existing item attribute information
-# When I update the information in the form and I click ‘submit’
-# Then I am redirected back to the item show page where I see the updated information
-# And I see a flash message stating that the information has been successfully updated.
-# 
     it "has existing item information" do
       visit edit_merchant_item_path(@merchant_1, @item_1)
       
@@ -32,31 +21,31 @@ RSpec.describe "Merchant Items Edit Page" do
       expect(page).to have_field("Current selling price", with: @item_1.unit_price)
     end
       
-
-      
-
     it "when visitor updates the information in the form and clicks submit, they are redirected back to item show page that displays updated information" do  
       visit edit_merchant_item_path(@merchant_1, @item_1)
-      
       
       fill_in("Name", with: "Nice Chair")
       fill_in("Description", with: "It's Nice")
       fill_in("Current selling price", with: 11888)
       click_button("Submit")
-      save_and_open_page
       
       expect(current_path).to eq(merchant_item_path(@merchant_1, @item_1))
+      expect(page).to have_content("Item updated successfully!")
       expect(page).to have_content("Nice Chair")
       expect(page).to have_content("It's Nice")
       expect(page).to have_content(11888)
-
-
-
-
     end
 
-    it "displays a flash message when the information has been successfully updated" do 
+    it "displays a flash message when the form is incomplete" do 
+      visit edit_merchant_item_path(@merchant_1, @item_1)
 
+      fill_in("Name", with: "")
+      fill_in("Description", with: "It's Nice")
+      fill_in("Current selling price", with: 11888)
+      click_button("Submit")
+
+      expect(current_path).to eq(edit_merchant_item_path(@merchant_1, @item_1))
+      expect(page).to have_content("Error updating item, incomplete form")
     end
   end
 end
