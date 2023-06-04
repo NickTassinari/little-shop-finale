be13b7ef2275cb695f9cec478ee40a3b810f2263
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-06-04 13:42:09 -0600

    remove tests

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -12,30 +12,4 @@ RSpec.describe Item, type: :model do
     it { should validate_presence_of :description }
     it { should validate_presence_of :unit_price }
   end
-
-  describe "Instance methods" do
-    before(:each) do 
-      @merchant_1 = create(:merchant)
-      @merchant_2 = create(:merchant)
-      @item_1 = create(:item, merchant: @merchant_1)
-      @item_2 = create(:item, merchant: @merchant_1) 
-      @item_3 = create(:item, merchant: @merchant_2) 
-    end
-
-    describe "#enable!" do
-      it "updates the status to enabled" do
-        @item_1.enable!
-
-        expect(@item_1.status).to eq("enabled")
-      end
-    end
-
-    describe "#disable!" do
-      it "updates the status to disabled" do
-        @item_2.disable!
-
-        expect(@item_2.status).to eq("disabled")
-      end
-    end
-  end
 end
