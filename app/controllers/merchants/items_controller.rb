995818a2a026b77193a51bfa25d6f54e185bbc1f
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-06-04 13:01:55 -0600

    add status update custom action

--- app/controllers/merchants/items_controller.rb
+++ app/controllers/merchants/items_controller.rb
@@ -25,9 +25,22 @@ class Merchants::ItemsController < ApplicationController
     end
   end
 
+  def status_update
+    @merchant = Merchant.find(params[:merchant_id])
+    @item = Item.find(params[:id])
+
+    if params[:commit] == "Enable"
+      @item.enable!
+    elsif params[:commit] == "Disable"
+      @item.disable!
+    end
+
+    redirect_to merchant_items_path(@merchant)
+  end
+
   private
 
   def item_params
-    params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
+    params.require(:item).permit(:name, :description, :unit_price, :merchant_id, :status)
   end
 end
