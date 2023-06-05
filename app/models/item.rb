43dc33346ff714e565acdf4928eee3adc34c012c
Steven Stackhouse <stackmm@gmail.com>
2023-06-05 12:52:39 -0600

    create num_sold and price_sold instance methods

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,12 @@ class Item < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   validates :unit_price, presence: true
+
+  def num_sold(invoice)
+    invoice_items.where(invoice_id: invoice.id).pluck(:quantity).first
+  end
+
+  def price_sold(invoice)
+    invoice_items.where(invoice_id: invoice.id).pluck(:unit_price).first
+  end
 end
