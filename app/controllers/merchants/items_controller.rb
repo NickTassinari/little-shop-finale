2b5028679902ac82b42b3837c7134f04774c68bc
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-06-05 15:15:44 -0600

    fix: call class methods into controller for access to view

--- app/controllers/merchants/items_controller.rb
+++ app/controllers/merchants/items_controller.rb
@@ -1,8 +1,9 @@
 class Merchants::ItemsController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
-    @enabled = @merchant.items.where(status: 1)
-    @disabled = @merchant.items.where(status: 0)
+    @disabled = Item.disabled_items
+    @enabled = Item.enabled_items
+    
   end
 
   def show
