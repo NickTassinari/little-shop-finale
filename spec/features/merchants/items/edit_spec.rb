e21642a49aa4a345c73e907dcd281af171b1a7e4
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-06-06 12:05:49 -0600

    fix tests for rendering what wireframe has, Current Price

--- spec/features/merchants/items/edit_spec.rb
+++ spec/features/merchants/items/edit_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe "Merchant Items Edit Page" do
       expect(page).to have_content("Item Edit Page")
       expect(page).to have_field("Name", with: @item_1.name)
       expect(page).to have_field("Description", with: @item_1.description)
-      expect(page).to have_field("Current selling price", with: @item_1.unit_price)
+      expect(page).to have_field("Current price", with: @item_1.unit_price)
     end
       
     it "when visitor updates the information in the form and clicks submit, they are redirected back to item show page that displays updated information" do  
@@ -26,7 +26,7 @@ RSpec.describe "Merchant Items Edit Page" do
       
       fill_in("Name", with: "Nice Chair")
       fill_in("Description", with: "It's Nice")
-      fill_in("Current selling price", with: 11888)
+      fill_in("Current price", with: 11888)
       click_button("Submit")
       
       expect(current_path).to eq(merchant_item_path(@merchant_1, @item_1))
@@ -41,7 +41,7 @@ RSpec.describe "Merchant Items Edit Page" do
 
       fill_in("Name", with: "")
       fill_in("Description", with: "It's Nice")
-      fill_in("Current selling price", with: 11888)
+      fill_in("Current price", with: 11888)
       click_button("Submit")
 
       expect(current_path).to eq(edit_merchant_item_path(@merchant_1, @item_1))
