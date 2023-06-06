3cbf8a16fd4f4ad6e1f822e97de659446693c8f0
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-06-06 12:21:42 -0600

    Test: add tests for the new item creation page

--- spec/features/merchants/items/new_spec.rb
+++ spec/features/merchants/items/new_spec.rb
@@ -10,13 +10,16 @@ RSpec.describe "Merchant Items New Page" do
     #US 11
     it "displays an empty form to create a new item" do
       visit new_merchant_item_path(@merchant_1)
+      save_and_open_page
 
       expect(page).to have_content("Create New Item")
       expect(page).to have_field("Name")
       expect(page).to have_field("Description")
+      expect(page).to have_field("Current Price")
+      expect(page).to have_button("Submit")
     end
 
-    it "user can fill ou t form to create a new item, click submit redirecting to the items index page" do
+    it "user can fill out form to create a new item, click submit redirecting to the items index page" do
 
     end
   end
