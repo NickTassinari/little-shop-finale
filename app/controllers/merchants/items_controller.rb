7b2070bb6cb9287106e01a5af8261f87df084aa6
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-06-05 20:59:58 -0600

    add new action to controller

--- app/controllers/merchants/items_controller.rb
+++ app/controllers/merchants/items_controller.rb
@@ -5,6 +5,10 @@ class Merchants::ItemsController < ApplicationController
     @enabled = Item.enabled_items    
   end
 
+  def new
+    
+  end
+
   def show
     @merchant = Merchant.find(params[:merchant_id])
     @item = Item.find(params[:id])
