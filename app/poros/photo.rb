6f75d8072cece07a2949ebb0cf291972071026f0
Steven Stackhouse <stackmm@gmail.com>
2023-06-07 08:51:30 -0600

    build photo object

--- app/poros/photo.rb
+++ app/poros/photo.rb
@@ -1,3 +1,19 @@
 class Photo
-  
+  attr_reader :data,
+              :description,
+              :small_url,
+              :full_url,
+              :likes,
+              :width,
+              :height
+              
+  def initialize(data)
+    @data = data
+    @description = data[:description]
+    @small_url = data[:urls][:small]
+    @full_url = data[:urls][:full]
+    @likes = data[:likes]
+    @width = data[:width]
+    @height = data[:height]
+  end
 end
