a62c1b57f6118491b4d28e2e1feec4fedde5f4e2
Steven Stackhouse <stackmm@gmail.com>
2023-06-07 13:52:47 -0600

    modify photo poro to not include attributes that arent used in app

--- app/poros/photo.rb
+++ app/poros/photo.rb
@@ -1,19 +1,13 @@
 class Photo
   attr_reader :data,
               :description,
-              :small_url,
-              :full_url,
-              :likes,
-              :width,
-              :height
-              
+              :url,
+              :likes
+
   def initialize(data)
     @data = data
     @description = data[:description]
-    @small_url = data[:urls][:small]
-    @full_url = data[:urls][:full]
+    @url = data[:urls][:small]
     @likes = data[:likes]
-    @width = data[:width]
-    @height = data[:height]
   end
 end
