4bcb6315f4b964cab4bdedc7f071e0b09f49bd8d
Nick Tassinari <ntass7800@gmail.com>
2023-06-09 14:16:41 -0600

    relationships with coupons

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,9 +1,11 @@
 class Invoice < ApplicationRecord
   belongs_to :customer
+  belongs_to :coupon, optional: true 
   has_many :transactions
   has_many :invoice_items
   has_many :items, through: :invoice_items
   has_many :merchants, through: :items
+
 
   enum status: {"in progress": 0, "completed": 1, "cancelled": 2}
 
