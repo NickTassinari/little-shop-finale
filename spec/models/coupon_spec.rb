13d8931d246c5b4602a4875d0aae777b2396669b
Nick Tassinari <ntass7800@gmail.com>
2023-06-09 17:34:08 -0600

    add test for validation of status

--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -3,11 +3,11 @@ require "rails_helper"
 RSpec.describe Coupon, type: :model do 
   before(:each) do 
     @merchant = Merchant.create!(name: "Ricky's Used Crap")
-    @coupon_1 = Coupon.create!(name: "BOGO 25% OFF", discount_type: "percentage", discount: 25, coupon_code: "Juneteenthbogo", merchant_id: @merchant.id)
-    @coupon_2 = Coupon.create!(name: "BOGO 40% OFF", discount_type: "percentage", discount: 40, coupon_code: "Independencebogo", merchant_id: @merchant.id)
-    @coupon_3 = Coupon.create!(name: "BOGO 50% OFF", discount_type: "percentage", discount: 50, coupon_code: "Laborbogo", merchant_id: @merchant.id)
-    @coupon_4 = Coupon.create!(name: "Twenty whole dollars OFF", discount_type: "dollars", discount: 20, coupon_code: "twentybills", merchant_id: @merchant.id)
-    @coupon_5 = Coupon.create!(name: "Forget About It Fridays", discount_type: "dollars", discount: 40, coupon_code: "Gabagool", merchant_id: @merchant.id)
+    @coupon_1 = Coupon.create!(name: "BOGO 25% OFF", discount_type: "percentage", discount: 25, coupon_code: "Juneteenthbogo", merchant_id: @merchant.id, status: "active")
+    @coupon_2 = Coupon.create!(name: "BOGO 40% OFF", discount_type: "percentage", discount: 40, coupon_code: "Independencebogo", merchant_id: @merchant.id, status: "active")
+    @coupon_3 = Coupon.create!(name: "BOGO 50% OFF", discount_type: "percentage", discount: 50, coupon_code: "Laborbogo", merchant_id: @merchant.id, status: "active")
+    @coupon_4 = Coupon.create!(name: "Twenty whole dollars OFF", discount_type: "dollars", discount: 20, coupon_code: "twentybills", merchant_id: @merchant.id, status: "active")
+    @coupon_5 = Coupon.create!(name: "Forget About It Fridays", discount_type: "dollars", discount: 40, coupon_code: "Gabagool", merchant_id: @merchant.id, status: "active")
   end
 
   describe "validations" do 
@@ -15,8 +15,10 @@ RSpec.describe Coupon, type: :model do
     it { should validate_presence_of :discount_type }
     it { should validate_presence_of :discount }
     it { should validate_presence_of :coupon_code }
+    it { should validate_presence_of :status }
     it { should validate_numericality_of :discount }
     it { should validate_uniqueness_of :coupon_code }
+
   end
 
   describe "relationships" do 
