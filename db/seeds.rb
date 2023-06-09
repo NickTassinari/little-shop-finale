7f7bca1c2d06009c48b2835f6e444cfe776e2a4a
Nick Tassinari <ntass7800@gmail.com>
2023-06-09 12:38:13 -0600

    render setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+{Model}.destroy_all 
 InvoiceItem.destroy_all
 Invoice.destroy_all
 Item.destroy_all
