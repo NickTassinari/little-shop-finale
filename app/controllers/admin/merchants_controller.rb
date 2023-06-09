52604c265c928846ea96d102a07fbdce9250dd9c
Nick Tassinari <ntass7800@gmail.com>
2023-06-09 15:57:10 -0600

    remove api

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -7,7 +7,7 @@ class Admin::MerchantsController < ApplicationController
 
   def show
     @merchant = Merchant.find(params[:id])
-    @merchant_random = PhotoBuilder.merchant_random
+    # @merchant_random = PhotoBuilder.merchant_random
   end
 
   def new
